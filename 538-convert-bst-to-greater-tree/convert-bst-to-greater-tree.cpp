38e8a8d5ace4d76e5bcd238d9866ffa828566605
Anujrai7987 <anujrai814@gmail.com>
2026-09-05 23:55:54 +0530

    Time: 0 ms (100.00%) | Memory: 34.9 MB (98.08%) - LeetSync

---
+++ 538-convert-bst-to-greater-tree/convert-bst-to-greater-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int sum = 0;
+
+    void solve(TreeNode* root) {
+        if (root == NULL)
+            return;
+
+        solve(root->right);
+
+        sum += root->val;
+        root->val = sum;
+
+        solve(root->left);
+    }
+
+    TreeNode* convertBST(TreeNode* root) {
+        solve(root);
+        return root;
+    }
+};
